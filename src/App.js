86387452a068a4bdffe681055c30da0527e9eea0
Chamas111 <ahmad.chamas87@gmail.com>
2023-02-07 17:06:33 +0100

    first comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+const contentful = require("contentful");
 
 function App() {
+  const [recepies, setRecepies] = useState([]);
+
+  const SPACE_ID = "5l97o5p6f7i4";
+  const ENVIRONMENT_ID = "master";
+  const ACCESS_TOKEN = "k5W8mi1k4jg_Y3Xpk87x73nREH0Chbeq6mNbvU175AE";
+
+  useEffect(() => {
+    const client = contentful.createClient({
+      space: SPACE_ID,
+
+      accessToken: ACCESS_TOKEN,
+      environment: ENVIRONMENT_ID,
+    });
+
+    client
+      .getEntries()
+      .then((result) => setRecepies(result.items))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {recepies.map((recepie) => (
+        <div key={recepie.sys.id}>
+          <h2>{recepie.fields.name}</h2>
+          <h3>{recepie.fields.ingridients}</h3>
+          <p>{recepie.fields.instructions}</p>
+          <p>{recepie.fields.descrption}</p>
+          {/*  <img
+            src={recepie.fields.image.fields.file.url}
+            alt="image"
+            style={{ width: "200px" }} */}
+        </div>
+      ))}
     </div>
   );
 }
