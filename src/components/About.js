7f49d561c0c0bdef6728b94e7c72f4a842502098
Chamas111 <ahmad.chamas87@gmail.com>
2023-02-10 15:57:45 +0100

    footer

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
   return (
     <div>
-      <h1>About</h1>
+      <h1>Contact Page under construction ;)</h1>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,8 @@ function Home() {
     <div className="hero">
       <div>
         <br></br>
-        <br></br>
-        <br></br>
-        <h4>
+
+        <h4 className="container">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s.
