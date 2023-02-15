7c9a20b8ff664c41dbae42a771ed9bf4d9aa4b9d
Chamas111 <ahmad.chamas87@gmail.com>
2023-02-15 18:06:07 +0100

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,110 @@
 import React from "react";
-
+import { MapContainer, TileLayer, useMap, Popup, Marker } from "react-leaflet";
+const position = [51.505, -0.09];
 function Contact() {
   return (
-    <div class="container">
-      <h1>Sry come back next Friday </h1>
-    </div>
+    <>
+      <section id="contact" className="contact">
+        <div className="container mt-4">
+          <div className="section-title">
+            <h2>
+              <em>Contact</em>
+            </h2>
+          </div>
+
+          <div className="row mt-5" data-aos="fade-in">
+            <div className="col-lg-5 d-flex align-items-stretch">
+              <div className="info">
+                <div className="address">
+                  <i className="bi bi-geo-alt"></i>
+                  <h4>Location:</h4>
+                  <p>Lorenzweg 5, 12099 Berlin</p>
+                </div>
+
+                <div className="email">
+                  <i className="bi bi-envelope"></i>
+                  <h4>Email:</h4>
+                  <p>info@PinchOfSalt.com</p>
+                </div>
+
+                <div className="phone">
+                  <i className="bi bi-phone"></i>
+                  <h4>Call:</h4>
+                  <p>+49 177 0000000</p>
+                </div>
+
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                  style={{ border: "0", width: "100%", height: "290px" }}
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+
+            <div className="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+              <form
+                action="forms/contact.php"
+                method="post"
+                role="form"
+                className="php-email-form"
+              >
+                <div className="row">
+                  <div className="form-group col-md-6">
+                    <label htmlFor="name">Your Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      className="form-control"
+                      id="name"
+                      required
+                    />
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label htmlFor="name">Your Email</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      name="email"
+                      id="email"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="name">Subject</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="subject"
+                    id="subject"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="name">Message</label>
+                  <textarea
+                    className="form-control"
+                    name="message"
+                    rows="10"
+                    required
+                  ></textarea>
+                </div>
+                <div className="my-3">
+                  <div className="loading">Loading</div>
+                  <div className="error-message"></div>
+                  <div className="sent-message">
+                    Your message has been sent. Thank you!
+                  </div>
+                </div>
+                <div className="text-center">
+                  <button type="submit">Send Message</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
